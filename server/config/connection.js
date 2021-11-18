ef74933a11550e88abeeed29c0168bc4b0adf681
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-11-17 19:09:53 -0500

    seeded db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jasongrossman:i1DDBbie8bEq3oet@cluster0.z3nv6.mongodb.net/shop-shop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
